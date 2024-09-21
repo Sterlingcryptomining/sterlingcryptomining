b89d6a4828e5b5d4acfa24d1277fddc1747f81a7
Big Elon <soft6dev@gmail.com>
2024-09-21 15:54:18 +0100

    best

--- src/main/java/com/soft6creators/futurespace/app/mailsender/MailSenderService.java
+++ src/main/java/com/soft6creators/futurespace/app/mailsender/MailSenderService.java
@@ -19,7 +19,7 @@ public class MailSenderService {
 	public void sendEmail(String toEmail, String subject, String body) throws MessagingException, UnsupportedEncodingException {
 		MimeMessage message = mailSender.createMimeMessage();
 		MimeMessageHelper helper = new MimeMessageHelper(message);
-		helper.setFrom("digitalbasecryptominers@gmail.com", "FX Base Crypto Miners");
+		helper.setFrom("digitalbasecryptominers@gmail.com", "Sterlingcryptomining");
 		helper.setTo(toEmail);
 		helper.setSubject(subject);
 		helper.setText(body, true);
